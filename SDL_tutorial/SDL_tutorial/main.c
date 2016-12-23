19d07d01d8ede45f70f850bcd9fa4967c840bab8
nsm <nick.hax@gmail.com>
2016-12-23 17:19:30 -0500

    initial

---
+++ SDL_tutorial/SDL_tutorial/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SDL_tutorial
+//
+//  Created by Nicolas Montanaro on 12/23/16.
+//  Copyright © 2016 Nicolas Montanaro. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
